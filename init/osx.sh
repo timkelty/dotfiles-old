0fcdee4663f90ff223542fc092bb4aa51c85995f
Tim Kelty <tim@fusionary.com>
2014-10-23 11:49:50 -0400

    Auto-correct

--- init/osx.sh
+++ init/osx.sh
@@ -90,6 +90,9 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # Disable press-and-hold for keys in favor of key repeat
 # defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
 ###############################################################################
 # Dock, Dashboard, and hot corners                                            #
 ###############################################################################
